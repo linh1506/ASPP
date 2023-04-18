364f9b3128a26f1141148b9d3aa7c37394d5eed1
d1agnoze <dacvuong202@gmail.com>
2023-04-18 16:20:19 +0700

    add data file

---
+++ Database/data.sql
@@ -0,0 +1,9 @@
+use DB_ODBG
+go
+insert into BRAND values ('Adidas')
+insert into BRAND values ('Nike')
+insert into BRAND values ('Puma')
+go
+insert into CATEGORY values ('Men')
+insert into CATEGORY values ('Women')
+insert into CATEGORY values ('Kids')
